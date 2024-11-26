bcac6f4393775468db5da4a25886733bd29c41bc
DzmitriyKaminskii <kaminskii.dmitriy@gmail.com>
2024-11-26 16:50:46 +0300

    Fix gradle and dart

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -4,33 +4,33 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:http/http.dart' as _i4;
-import 'package:injectable/injectable.dart' as _i2;
+import 'package:get_it/get_it.dart' as i1;
+import 'package:http/http.dart' as i4;
+import 'package:injectable/injectable.dart' as i2;
 
-import 'features/list_news/data/datasources/news_data_sources.dart' as _i3;
-import 'features/list_news/data/repositories/news_repository_impl.dart' as _i6;
-import 'features/list_news/domain/repositories/news_repository.dart' as _i5;
-import 'features/list_news/domain/usercases/get_news.dart' as _i9;
+import 'features/list_news/data/datasources/news_data_sources.dart' as i3;
+import 'features/list_news/data/repositories/news_repository_impl.dart' as i6;
+import 'features/list_news/domain/repositories/news_repository.dart' as i5;
+import 'features/list_news/domain/usercases/get_news.dart' as i9;
 import 'features/list_news/presentation/bloc/list_news/list_news_bloc.dart'
-    as _i10;
-import 'features/list_news/presentation/bloc/search/search_bloc.dart' as _i7;
+    as i10;
+import 'features/list_news/presentation/bloc/search/search_bloc.dart' as i7;
 import 'features/list_news/presentation/bloc/tabs_bar/tabs_bar_bloc.dart'
-    as _i8; // ignore_for_file: unnecessary_lambdas
+    as i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
-_i1.GetIt $initGetIt(_i1.GetIt get,
-    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
-  final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.NewsDataSources>(
-      () => _i3.NewsDataSourcesImpl(client: get<_i4.Client>()));
-  gh.factory<_i5.NewsRepository>(() =>
-      _i6.NewsRepositoryImpl(newsDataSources: get<_i3.NewsDataSources>()));
-  gh.factory<_i7.SearchBloc>(() => _i7.SearchBloc());
-  gh.factory<_i8.TabsBarBloc>(() => _i8.TabsBarBloc());
-  gh.factory<_i9.GetNews>(() => _i9.GetNews(get<_i5.NewsRepository>()));
-  gh.factory<_i10.ListNewsBloc>(
-      () => _i10.ListNewsBloc(getNews: get<_i9.GetNews>()));
+i1.GetIt $initGetIt(i1.GetIt get,
+    {String? environment, i2.EnvironmentFilter? environmentFilter}) {
+  final gh = i2.GetItHelper(get, environment, environmentFilter);
+  gh.factory<i3.NewsDataSources>(
+      () => i3.NewsDataSourcesImpl(client: get<i4.Client>()));
+  gh.factory<i5.NewsRepository>(() =>
+      i6.NewsRepositoryImpl(newsDataSources: get<i3.NewsDataSources>()));
+  gh.factory<i7.SearchBloc>(() => i7.SearchBloc());
+  gh.factory<i8.TabsBarBloc>(() => i8.TabsBarBloc());
+  gh.factory<i9.GetNews>(() => i9.GetNews(get<i5.NewsRepository>()));
+  gh.factory<i10.ListNewsBloc>(
+      () => i10.ListNewsBloc(getNews: get<i9.GetNews>()));
   return get;
 }
